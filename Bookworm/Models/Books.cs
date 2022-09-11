cf159580e6acdf04eeabd33d579d6f8ee51710b7
Adrienne <100542795+ampando@users.noreply.github.com>
2022-09-11 11:23:41 -0700

    initial project setup.

---
+++ Bookworm/Models/Books.cs
@@ -0,0 +1,10 @@
+namespace Bookworm.Models
+{
+    public class Books
+    {
+        public int AnimalId { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int Age { get; set; }
+    }
+}
